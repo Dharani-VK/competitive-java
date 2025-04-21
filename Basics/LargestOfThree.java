a304d28f01b7868a6d1953093b1e579dc51b259a
Dharani V K <dharani.vk05@gmail.com>
2025-04-21 20:37:39 +0530

    Create LargestOfThree.java
    This file contains Java code to find the largest among 3 numbers.

---
+++ Basics/LargestOfThree.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class largest_of_3_num{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number 1 : ");
+        int num1 = sc.nextInt();
+        System.out.print("Enter number 2 : ");
+        int num2 = sc.nextInt();
+        System.out.print("Enter number 3 : ");
+        int num3 = sc.nextInt();
+        if ((num1 > num2 ) && (num1 > num3)){
+            System.out.println("Number 1 ( " +num1 +" ) is Greater");
+        }
+        else if ((num2 > num1) && (num2 >num3)){
+            System.out.println("Number 2 ( " +num2 +  " ) is Greater");
+        }
+
+        else {
+            System.out.println("Number 3 ( " +num3 +" ) is Greater");
+        }
+    }
+}
