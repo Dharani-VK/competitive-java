a0d18c6d0748417b8ab1756e37b78b8df6147fff
Dharani V K <dharani.vk05@gmail.com>
2025-04-21 20:42:52 +0530

    Update LargestOfThree.java
    Find the largest among three numbers using if-else.

--- Basics/LargestOfThree.java
+++ Basics/LargestOfThree.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class largest_of_3_num{
+public class LargestOfThree{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter number 1 : ");
